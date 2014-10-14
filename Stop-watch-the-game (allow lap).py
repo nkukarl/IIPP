e23e0ea616b65c437b3617a765ba887955518ada
nkukarl <nkukarl@users.noreply.github.com>
2014-10-14 17:43:23 +1100

    Update Stop-watch-the-game (allow lap).py

--- Stop-watch-the-game (allow lap).py
+++ Stop-watch-the-game (allow lap).py
@@ -5,6 +5,10 @@ t = 0
 interval = 100
 running_state = False
 lapped_time = []
+lapped_head_pos = 30
+max_laps = 15
+WIDTH = 500
+HEIGHT = 300
 
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
@@ -18,7 +22,13 @@ def format(t):
     return A + ":" + BC + "." + D
 
 def format_id(i):
-    return '#' + str(i)
+    if len(lapped_time) < 10:
+        return '#' + str(i)
+    elif len(lapped_time) < 100:
+        if len(str(i)) == 1:
+            return '#0' + str(i)
+        else:
+            return '#' + str(i)
    
 # define event handlers for buttons; "Start", "Stop", "Reset"
 def start():
@@ -41,7 +51,7 @@ def reset():
     
 def lap():
     global lapped_time
-    if running_state:
+    if running_state and len(lapped_time) < max_laps:        
         lapped_time.append(format(t))
 
 # define event handler for timer with 0.1 sec interval
@@ -51,19 +61,30 @@ def tick():
 
 # define draw handler
 def draw(canvas):
-    canvas.draw_text(format(t), [40, 40], 30, "red")
-    lapped_head = 100
-    for i in range(len(lapped_time)):
-        canvas.draw_text(format_id(i + 1), [10, lapped_head], 30, "white")
-        canvas.draw_text(lapped_time[i], [80, lapped_head], 30, "white")
-        lapped_head += 30
+    global lapped_head_pos
+    canvas.draw_text(format(t), [30, 30], 30, "red")
+    for i in range(min(len(lapped_time), max_laps)):
+        canvas.draw_text(format_id(i + 1), [200, lapped_head_pos + 30 * i], 30, "white")
+        canvas.draw_text(lapped_time[i], [280, lapped_head_pos  + 30 * i], 30, "white")
 
+def key_down(key):
+    global vel, lapped_head_pos
+    if key == simplegui.KEY_MAP['down']:
+        if lapped_head_pos + 30 * (len(lapped_time) - 1)  > HEIGHT:
+            lapped_head_pos -= 30
+    elif key == simplegui.KEY_MAP['up']:
+        if lapped_head_pos < 30:
+            lapped_head_pos += 30
+        
 # create frame
-frame = simplegui.create_frame("Stopwatch", 200, 975)
+frame = simplegui.create_frame("Stopwatch", WIDTH, HEIGHT)
 
 # register event handlers
 frame.set_draw_handler(draw)
+frame.set_keydown_handler(key_down)
+
 timer = simplegui.create_timer(interval, tick)
+
 frame.add_button("Start", start, 100)
 frame.add_button("Stop", stop, 100)
 frame.add_button("Reset", reset, 100)
